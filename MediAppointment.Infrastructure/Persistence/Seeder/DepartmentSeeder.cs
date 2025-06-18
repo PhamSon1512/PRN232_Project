e0d62530135f457247fea2b108b885651ffe33a4
Nguyen Huy <a1k28cbq@gmail.com>
2025-06-18 17:05:54 +0700

    Fix SeedData

--- MediAppointment.Infrastructure/Persistence/Seeder/DepartmentSeeder.cs
+++ MediAppointment.Infrastructure/Persistence/Seeder/DepartmentSeeder.cs
@@ -21,17 +21,17 @@ namespace MediAppointment.Infrastructure.Persistence.Seeder
                 {
                     new Department
                     {
-                        Id = Guid.NewGuid(),
+                        Id = Guid.Parse("0A438320-EEFE-4DB7-9DCA-51A17AB9AEE5"),
                         DepartmentName = "Cardiology"
                     },
                     new Department
                     {
-                        Id = Guid.NewGuid(),
+                        Id = Guid.Parse("4D484610-3623-465D-82C2-55C0337A5A11"),
                         DepartmentName = "Neurology"
                     },
                     new Department
                     {
-                        Id = Guid.NewGuid(),
+                        Id = Guid.Parse("E053F9BF-3195-4A42-904A-E99A615BE701"),
                         DepartmentName = "Pediatrics"
                     }
                 };
