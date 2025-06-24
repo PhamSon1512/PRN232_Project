98c5ae0771eee96a377fd5c1bb18c14df0e81b27
Nguyen Huy <a1k28cbq@gmail.com>
2025-06-24 17:50:58 +0700

    Complete DoctorSchedule

---
+++ MediAppointment.API/Controllers/DoctorScheduleController.cs
@@ -0,0 +1,39 @@
+﻿using MediAppointment.Application.DTOs.DoctorScheduleDTOs;
+using MediAppointment.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediAppointment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorScheduleController : ControllerBase
+    {
+        protected IDoctorScheduleService _service;
+        public DoctorScheduleController(IDoctorScheduleService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(List<DoctorScheduleRequest> requests) {
+            /*var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (Guid.TryParse(userIdClaim, out Guid id))
+                {
+                }*/
+            Guid id = Guid.Parse("497DD1AE-1135-4169-B8EA-ED3485C6C2CB");
+            await _service.CreateDoctorSchedule(id, requests);
+            return Ok();
+        }
+        [HttpPost("DeleteRoomTimeSlot")]
+        public async Task<IActionResult> Get(Guid RoomTimeSlotId)
+        {
+            /*var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (Guid.TryParse(userIdClaim, out Guid id))
+                {
+                }*/
+            Guid id = Guid.Parse("497DD1AE-1135-4169-B8EA-ED3485C6C2CB");
+            await _service.DeleteDoctorSchedule(RoomTimeSlotId);
+            return Ok();
+        }
+    }
+}
